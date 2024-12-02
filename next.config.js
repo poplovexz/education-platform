9b7c4552019f2247c15dad87f09ef3529adb765a
poplovexz <129678290+poplovexz@users.noreply.github.com>
2024-12-02 16:04:25 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
+}
+
+export default nextConfig
