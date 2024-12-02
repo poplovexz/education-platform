ad40b77afab39015cb454c25baaa957b1dbd3786
poplovexz <129678290+poplovexz@users.noreply.github.com>
2024-12-02 16:09:41 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Education Platform</title>
+        <meta name="description" content="An education platform for students" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to our Education Platform</h1>
+      </main>
+    </div>
+  )
+}
